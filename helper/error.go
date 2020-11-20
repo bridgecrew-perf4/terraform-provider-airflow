98e34a25f69db5318f2108d0a555f44c9ab21927
Xabier Napal <xabiernapal@pm.me>
2020-11-20 21:26:21 +0100

    refactor: improve response error handling

--- helper/error.go
+++ helper/error.go
@@ -1,52 +1,90 @@
 package helper
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"reflect"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/xabinapal/terraform-provider-airflow/api"
 )
 
-func GetResponseDiag(res interface{}) *diag.Diagnostic {
-	obj := reflect.ValueOf(res).Elem()
+func GetResponseDiag(obj interface{}) *diag.Diagnostic {
+	res := parseResponse(obj)
 
-	statusFn := obj.MethodByName("StatusCode")
-	if !statusFn.IsValid() {
-		panic("Invalid response object")
+	if res.HTTPResponse == nil {
+		return &diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  "0 - Unknown error",
+			Detail:   "HTTPResponse field is nil",
+		}
 	}
 
-	statusVal := statusFn.Call([]reflect.Value{})
-	if len(statusVal) != 1 || !statusVal[0].IsValid() ||
-		statusVal[0].Kind() != reflect.Int {
-		panic("Invalid response object")
-	}
-
-	status := statusVal[0].Interface().(int)
+	status := res.HTTPResponse.StatusCode
 	if status == 200 {
 		return nil
 	}
 
-	errVal := obj.FieldByName(fmt.Sprintf("JSON%d", status))
-	if !errVal.IsValid() {
+	apiErr, err := parseErrorResponse(res)
+	if err != nil {
 		return &diag.Diagnostic{
 			Severity: diag.Error,
 			Summary:  fmt.Sprintf("%d - Unknown error", status),
+			Detail:   err.Error(),
 		}
-	} else if errVal.Type() != reflect.TypeOf((*api.Error)(nil)) {
-		panic("Invalid response object")
 	}
 
-	err := errVal.Interface().(*api.Error)
-
 	var detail string
-	if err.Detail != nil {
-		detail = *err.Detail
+	if apiErr.Detail != nil {
+		detail = *apiErr.Detail
 	}
 
 	return &diag.Diagnostic{
 		Severity: diag.Error,
-		Summary:  fmt.Sprintf("%d - %s", status, err.Title),
+		Summary:  fmt.Sprintf("%d - %s", status, apiErr.Title),
 		Detail:   detail,
 	}
+}
+
+type response struct {
+	HTTPResponse *http.Response
+	Body         []byte
+}
+
+func parseResponse(res interface{}) *response {
+	obj := reflect.ValueOf(res).Elem()
+
+	resObj := obj.FieldByName("HTTPResponse")
+	if !resObj.IsValid() {
+		panic("Invalid response object")
+	}
+
+	bodyObj := obj.FieldByName("Body")
+	if !bodyObj.IsValid() {
+		panic("Invalid response object")
+	}
+
+	return &response{
+		HTTPResponse: resObj.Interface().(*http.Response),
+		Body:         bodyObj.Interface().([]byte),
+	}
+}
+
+func parseErrorResponse(res *response) (*api.Error, error) {
+	if !strings.Contains(res.HTTPResponse.Header.Get("Content-Type"), "json") {
+		return nil, fmt.Errorf("HTTPResponse field is not JSON")
+	}
+
+	if res.Body == nil {
+		return nil, fmt.Errorf("Body field is nil")
+	}
+
+	var dest api.Error
+	if err := json.Unmarshal(res.Body, &dest); err != nil {
+		return nil, err
+	}
+
+	return &dest, nil
 }
